891aaf03f24a2a6d7b629d98bc7be73ad49cba7b
HENG ZHANG <yugiyx@163.com>
2018-11-06 18:38:07 +0800

    modify

--- instagram/instagram/spiders/images.py
+++ instagram/instagram/spiders/images.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
+import hashlib
 import scrapy
 from ..items import ImageItem
 
@@ -12,17 +13,23 @@ class ImagesSpider(scrapy.Spider):
     def parse(self, response):
         images = ImageItem()
         json_data = json.loads(response.css('script')[3].extract()[52:-10])
-        json_data = json_data["entry_data"]["ProfilePage"][0]["graphql"]["user"]["edge_owner_to_timeline_media"]
-        items = json_data["page_info"]
-        end_cursor = items['end_cursor']
-        has_next_page = items['has_next_page']
-        items = json_data["edges"]
+        json_data = json_data["entry_data"]["ProfilePage"][0][
+            "graphql"]["user"]["edge_owner_to_timeline_media"]
+        page_info = json_data["page_info"]
+        end_cursor = page_info['end_cursor']
+        has_next_page = page_info['has_next_page']
+        edges = json_data["edges"]
         # images['id'] = items[0]['node']['owner']['id']
-        images['title'] = items[0]['node']['owner']['username']
-        images['image_urls'] = [item['node']['display_url'] for item in items]
-        return images
+        images['title'] = edges[0]['node']['owner']['username']
+        images['image_urls'] = [item['node']['display_url'] for item in edges]
+        yield images
 
         if has_next_page:
-            next_url = 
-query_hash = '5b0222df65d7f6659c9b82246780caa7'
-variables = {"id": "1413437914", "first": 50, "after": "QVFDVzFIbWc5dkRhcmtNcEl2VEt2bnZOZS14bmgzYnk1MkR1RElZUm85MlBvTFJ5Qkp1RG5CbjFEZHBsb1ZwQV9jSEo3ZTkxamVHREVlcmEtcGx2ODFWag=="}
+            print('还有后文')
+            print(end_cursor)
+            yield scrapy.Request(next_url, headers=headers)
+
+    def hash_str(self, strInfo):
+        h = hashlib.md5()
+        h.update(strInfo.encode("utf-8"))
+        return h.hexdigest()
